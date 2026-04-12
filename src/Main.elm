6be85bb46af6a9286f79bcf6b6b6f3705ebffa6f
RaySlash <stevemathewjoy@tutanota.com>
2026-04-12 19:00:02 +1000

    update: add home items and footer

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, header, footer, button, div, text, a, img)
+import Html exposing (Html, h1, header, footer, button, div, text, a, img)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Pages exposing (homePage, aboutUsPage, newsPage)
@@ -17,35 +17,64 @@ update : Msg -> Model -> Model
 update msg model =
   case msg of
     SwitchToPage new_page ->
-            {model | page = new_page}
-             
+      {model | page = new_page}
+
 view : Model -> Html Msg
 view model =
-    div [class "body"] [ 
-         header [class "header"]
-             [
-              div [id "nav-logo"] [
-                   div [] [text "BNE"]
-                  ,div [] [text "CLEANERS"]
-              ],
-              div [id "nav-items"] [
-                       button [onClick (SwitchToPage Home)
-                              ,(if model.page == Home then class "nav-item-active" else class "nav-item")] [text "Home"]
-                      ,button [onClick (SwitchToPage News)
-                              ,(if model.page == News then class "nav-item-active" else class "nav-item")] [text "News"]
-                      ,button [onClick (SwitchToPage AboutUs)
-                              ,(if model.page == AboutUs then class "nav-item-active" else class "nav-item")] [text "About Us"]
-                  ],
-              div [id "nav-right"] [button [id "book-a-clean-btn"] [text "Book a Clean"]]    
-      ],
---updates to homepage
-        (if model.page == Home then homePage model
---updates to about Us page
-         else if model.page == AboutUs then aboutUsPage model
-         else if model.page == News then newsPage model
-         else div [][]),
-        footer [class "footer"] [
-            div [class "footer-text"] [text "Copyright Reserved 2026"]
+  div [class "body"] [ 
+    header [class "header"]
+    [
+        div [id "nav-logo"] [
+        div [] [text "BNE"]
+        ,div [] [text "CLEANERS"]
+        ],
+        div [id "nav-items"] [
+          button [onClick (SwitchToPage Home)
+          ,(if model.page == Home then class "nav-item-active" else class "nav-item")] [text "Home"]
+          ,button [onClick (SwitchToPage News)
+          ,(if model.page == News then class "nav-item-active" else class "nav-item")] [text "News"]
+          ,button [onClick (SwitchToPage AboutUs)
+          ,(if model.page == AboutUs then class "nav-item-active" else class "nav-item")] [text "About Us"]
+          ],
+          div [id "nav-right"] [button [id "book-a-clean-btn"] [text "Book a Clean"]]]
+          ,div [class "hero"] [
+            img [id "hero-image"
+            ,src "./static/glass_clean.jpg"
+            ,alt "Hero image"] []
+            ]
+          ,(if model.page == Home then homePage model
+          else if model.page == AboutUs then aboutUsPage model
+          else if model.page == News then newsPage model
+          else div [][])
+          ,footer [class "footer"] [
+            div [class "footer-items"] [
+              div [class "footer-item"] [
+                h1 [] [text "Services"]
+                , div [class "footer-links"] [
+                  a [href "#"] [text "Regular Cleaning"]
+                  ,a [href "#"] [text "Deep Cleaning"]
+                  ,a [href "#"] [text "Inspection Cleaning"]
+                  ,a [href "#"] [text "Bond Cleaning"]
+                  ,a [href "#"] [text "Office Cleaning"]
+                  ,a [href "#"] [text "Carpet Steam Cleaning"]
+                  ]
                 ]
+                ,div [class "footer-item"] [
+                  h1 [] [text "Quick Links"]
+                  , div [class "footer-links"] [
+                    a [href "#"] [text "Privacy Policy"]
+                    ,a [href "#"] [text "Terms Of Services"]
+                    ,a [href "#"] [text "FAQ"]
+                    ,a [href "#"] [text "About Us"]
+                    ,a [href "#"] [text "Contact Us"]
+                    ,a [href "#"] [text "Become A Partner"]
+                    ]
+                ]
+              ]
+            ,div [class "footer-text"] [
+              div [] [text "Copyright Reserved 2026"]
+              , div [] [text "BNE Cleaners. All rights reserved."]
+              ]
+            ]
 
         ]
--- src/Pages.elm
+++ src/Pages.elm
@@ -6,12 +6,43 @@ import Types exposing (Model, Msg)
 
 
 homePage: Model -> Html Msg
-homePage model =
-    div [class "hero"] [
-         img [id "hero-image"
-             ,src "./static/glass_clean.jpg"
-             ,alt "Hero image"] []
-        ]
+homePage model = div [class "home-container"] [
+  div [class "home-bubble"] [
+    div [class "home-bubble-logo"] [
+        text "X"
+      ]
+    , div [class "home-bubble-text"] [text "Police-Checked Cleaners"]
+    , div [class "home-bubble-text"] [text "Selected for respectful in-home residential work, with trust and professionalism expected from the start."]
+    ]
+  , div [class "home-bubble"] [
+    div [class "home-bubble-logo"] [
+        text "X"
+      ]
+    , div [class "home-bubble-text"] [text "Defined Cleaning Scopes"]
+    , div [class "home-bubble-text"] [text "A structured services approach that helps keep the scope clearer and the deliverfy more consistent."]
+    ]
+  , div [class "home-bubble"] [
+    div [class "home-bubble-logo"] [
+        text "X"
+      ]
+    , div [class "home-bubble-text"] [text "Easy Online Booking"]
+    , div [class "home-bubble-text"] [text "Clear pricing, fast quoting, and a staightforward booking flow designed to make planning easier."]
+    ]
+  , div [class "home-bubble"] [
+    div [class "home-bubble-logo"] [
+        text "X"
+      ]
+    , div [class "home-bubble-text"] [text "Training Backed Delivery"]
+    , div [class "home-bubble-text"] [text "Our service model is built around defined methods and expectations, not just individual habit or guesswork."]
+    ]
+  , div [class "home-bubble"] [
+    div [class "home-bubble-logo"] [
+        text "X"
+      ]
+    , div [class "home-bubble-text"] [text "Commercial Standards"]
+    , div [class "home-bubble-text"] [text "Our scope always follow all conventions proposed by the Australian Government and according to community standards."]
+    ]
+  ]
 
 
 newsPage: Model -> Html Msg
@@ -19,49 +50,31 @@ newsPage model = div [] []
 
 aboutUsPage: Model -> Html Msg
 aboutUsPage model = 
-    
-    div [] [
-        div[class "hero"][
-            img [id "hero-image"
-                ,src "./static/glass_clean.jpg"
-                ,alt "Hero image"][]
-        ],
-        div[class "aboutus-container"][
-            div[class "review-post"][
+    div [] [ div [class "aboutus-container"][
+          div [class "review-post"][
+            img [id "profile-image"
+            ,src "./static/demo_pfp.png"
+            ,alt "profile image"][]
+            ,div [ class "review-text" ][
+              text "Caption text" ]],
+              div[class "review-post"][
+                img [id "profile-image"
+                , src "./static/demo_pfp.png"
+                , alt "profile image"][]
+                , div [class "review-text"][
+                   text "Caption text" ]]
+              , div[class "review-post"][
                     img [id "profile-image"
+                    ,src "./static/demo_pfp.png"
+                    ,alt "profile image"][]
+                    ,div[class "review-text"][
+                      text "Caption text" ]] 
+              , div[class "review-post"][
+                        img [id "profile-image"
                         ,src "./static/demo_pfp.png"
                         ,alt "profile image"][]
-                    ,div[class "review-text"][
-                        text "this needs to be linked to google gsfdgsfg fsdgsfgs sfgsfdgs review database or smth"
-                        ]
-                    ],
-            div[class "review-post"][
-                    img [id "profile-image"
-                        ,src "./static/demo_pfp.png"
-                        ,alt "profile image"][]
-                    ,div[class "review-text"][
-                        text "this needs to be linked to google gsfdgsfg fsdgsfgs sfgsfdgs review database or smth"
-                        ]
-                    ]
-                    ,            div[class "review-post"][
-                    img [id "profile-image"
-                        ,src "./static/demo_pfp.png"
-                        ,alt "profile image"][]
-                    ,div[class "review-text"][
-                        text "this needs to be linked to google gsfdgsfg fsdgsfgs sfgsfdgs review database or smth"
-                        ]
-                    ] 
-                    ,            div[class "review-post"][
-                    img [id "profile-image"
-                        ,src "./static/demo_pfp.png"
-                        ,alt "profile image"][]
-                    ,div[class "review-text"][
-                        text "this needs to be linked to google gsfdgsfg fsdgsfgs sfgsfdgs review database or smth"
-                        ]
-                    ]          
-                ]
-            ]
-            
+                        ,div[class "review-text"][
+                          text "Caption text" ]]]]
 
 
 -- Needs to be a form which takes User details.
